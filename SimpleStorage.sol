c96272a04ebeb15ad552ec815215688faee617b9
danialjankouk <dani.jankook@gmail.com>
2026-01-05 01:26:15 +0330

    types

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,26 @@
 pragma solidity 0.8.18;
 
 contract Storage {
+// functions
+    uint256 public favoritnumber;
+    // public   =>  can use in othere contracts 
+    // private   =>  just can use in this contract
+    // internal  =>  just this contract and childrens
+    // external  =>  just outside of this contract
+    // as default => internal
+    function store(uint256 _favNumber) public {
+        favoritnumber = _favNumber + 1;
+    }
+
+    //-------
     
+    address wallet = 0x460598e7Bbb61f4155841A8Ce3A8452E5348aDBC;
+    //pure vs view
+    function set() public view returns (address){
+        return wallet;
+    }
+    function get(uint256 a,uint256 b) public pure returns (uint256){
+        return a + b;
+    }
 }
+
---
+++ base.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+// example of types
+
+    // public   =>  can use in othere contracts 
+    // private   =>  just can use in this contract
+    // internal  =>  just this contract and childrens
+    // external  =>  just outside of this contract
+    // as default => internal
+    
+contract Base {
+    uint256 internal number = 10;
+    // internal
+    function intr() internal view returns (uint256){
+        return number;
+    }
+
+    function extrn() external view returns (uint256){
+        return number;
+    }
+
+    function publk() public view returns (uint256) {
+        return number;
+    }
+
+    function prvt(uint256 _a) private view returns (uint256){
+        return number + _a;
+    }
+}
